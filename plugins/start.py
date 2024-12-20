f9d959a035e5555555ed80782864b94f65b98ddd
dy3no <165803560+dy3no@users.noreply.github.com>
2024-12-20 22:32:37 +0530

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,57 @@
+import os
+from pyrogram.errors import ChatAdminRequired , FloodWait
+import random
+import asyncio
+from pyrogram.types import InlineKeyboardButton , InlineKeyboardMarkup , CallbackQuery
+from pyrogram import enums , filters , Client
+from info import API_ID , API_HASH , BOT_TOKEN , PORT , ADMINS , LOG_CHANNEL , DATABASE_NAME , DATABASE_URI
+from Script import script
+import time
+from utils import temp , get_size
+from pyrogram.errors import FloodWait
+from database.users_db import db
+import re
+import json
+import base64
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+@Client.on_message(filters.command("start") & filters.incoming)
+async def start(client , message):
+    if message.chat.type in [enums.ChatType.GROUP , enums.ChatType.SUPERGROUP]:
+        button = [[InlineKeyboardButton("🔸 ɢʀᴏᴜᴩ 1 🔸" , url="https://t.me/+q9PpzTvYD882OTU1") ,
+                   InlineKeyboardButton("🔸 ɢʀᴩᴜᴩ 2 🔸" , url="https://t.me/+q9PpzTvYD882OTU1")
+                   ] , [
+                      InlineKeyboardButton("🔸 ᴄʜᴀɴɴᴇʟ 1 🔸" , url="https://t.me/+1AtPM7FXm_pjNDA9") ,
+                      InlineKeyboardButton("🔸 ᴄʜᴀɴɴᴇʟ 2 🔸" , url="https://t.me/+v5I41M1IIH5iYzll")
+                  ] , [
+                      InlineKeyboardButton("🔸 ᴍᴏᴠɪᴇ ʀᴇqᴜᴇꜱᴛɪɴɢ ʙᴏᴛ 🔸" , url="https://t.me/Movie_Requesting_Robot")
+                  ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply(text=script.START_TXT.format(message.from_user.mention , temp.U_NAME , temp.B_NAME) ,
+                            reply_markup=reply_markup)
+        if not await db.get_chat(message.chat.id):
+            total=await client.get_chat_members_count(message.chat.id)
+            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))
+            await db.add_chat(message.chat.id, message.chat.title)
+        return 
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id , message.from_user.first_name)
+        await client.send_message(LOG_CHANNEL ,
+                                  script.LOG_TEXT_P.format(message.from_user.id , message.from_user.mention ,
+                                                           message.from_user.id))
+    if len(message.command) != 2:
+            mine = await client.get_me()
+            button = [[InlineKeyboardButton("🔸 ɢʀᴏᴜᴩ 1 🔸" , url="https://t.me/+q9PpzTvYD882OTU1") ,
+                       InlineKeyboardButton("🔸 ɢʀᴩᴜᴩ 2 🔸" , url="https://t.me/+q9PpzTvYD882OTU1")
+                       ] , [
+                          InlineKeyboardButton("🔸 ᴄʜᴀɴɴᴇʟ 1 🔸" , url="https://t.me/+1AtPM7FXm_pjNDA9") ,
+                          InlineKeyboardButton("🔸 ᴄʜᴀɴɴᴇʟ 2 🔸" , url="https://t.me/+v5I41M1IIH5iYzll")
+                      ] , [
+                          InlineKeyboardButton("🔸 ᴍᴏᴠɪᴇ ʀᴇqᴜᴇꜱᴛɪɴɢ ʙᴏᴛ 🔸" , url="https://t.me/Movie_Requesting_Robot")
+                      ]]
+            await client.send_message(chat_id=message.chat.id ,
+                                      text=f"__Hello {message.from_user.mention} Iam Auto Approver Join Request Bot Just [Add Me To Your Group or Channnl](http://t.me/{mine.username}?startgroup=botstart)__" ,
+                                      reply_markup=InlineKeyboardMarkup(button) , disable_web_page_preview=True)
